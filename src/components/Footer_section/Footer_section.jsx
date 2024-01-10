89b4ac9bf97f3856dda347905b38b6cfaac0db85
Lee <lgitonga101@gmail.com>
2024-01-10 21:54:06 +0300

    modified:   src/components/Footer_section/Footer_section.css 	modified:   src/components/Footer_section/Footer_section.jsx

--- src/components/Footer_section/Footer_section.jsx
+++ src/components/Footer_section/Footer_section.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Footer_section.css'
 
 const Footer_section = () => {
   return (
     <div>
-      <h1>Footer</h1>
+       <footer class="white-top-border text-light mt-4 py-3">
+        <div class="container text-center">
+          <h4 class="logo custom__logo__styling">Contact Me</h4>
+              <nav class="nav justify-content-center">
+                <i class="fa-brands fa-github fa-brand-override"></i>
+                <a class="nav-link custom__navigation__links" href="https://github.com/calmcat1">Github</a>
+
+                <i class="fa-brands fa-linkedin fa-brand-override"></i>
+                <a class="nav-link custom__navigation__links" href="https://www.linkedin.com/in/lee-gitonga-3a5921242/">LinkedIn</a>
+
+                <i class="fa-regular fa-envelope fa-brand-override"></i>
+                <a class="nav-link custom__navigation__links" href="/mailto:tarafikicom@gmail.com">Email</a>
+              </nav>
+        </div>
+      </footer>
     </div>
   )
 }
