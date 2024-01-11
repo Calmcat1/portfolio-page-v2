64efb3bd57eae04815a0aea4784d260d2e492baa
Lee <lgitonga101@gmail.com>
2024-01-11 19:39:07 +0300

    modified:   src/App.css modified:   src/components/Footer_section/Footer_section.jsx

--- src/components/Footer_section/Footer_section.jsx
+++ src/components/Footer_section/Footer_section.jsx
@@ -9,13 +9,13 @@ const Footer_section = () => {
           <h4 class="logo custom__logo__styling">Contact Me</h4>
               <nav class="nav justify-content-center">
                 <i class="fa-brands fa-github fa-brand-override"></i>
-                <a class="nav-link custom__navigation__links" href="https://github.com/calmcat1">Github</a>
+                <a class="nav-link custom__navigation__links clickable__links" href="https://github.com/calmcat1">Github</a>
 
                 <i class="fa-brands fa-linkedin fa-brand-override"></i>
-                <a class="nav-link custom__navigation__links" href="https://www.linkedin.com/in/lee-gitonga-3a5921242/">LinkedIn</a>
+                <a class="nav-link custom__navigation__links clickable__links" href="https://www.linkedin.com/in/lee-gitonga-3a5921242/">LinkedIn</a>
 
                 <i class="fa-regular fa-envelope fa-brand-override"></i>
-                <a class="nav-link custom__navigation__links" href="mailto:tarafikicom@gmail.com">Email</a>
+                <a class="nav-link custom__navigation__links clickable__links" href="mailto:tarafikicom@gmail.com">Email</a>
               </nav>
         </div>
       </footer>
